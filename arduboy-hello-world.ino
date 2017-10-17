afc03e1c797fd5028f259824bf2027fd29ee9fc0
Jesse Millar <hellojessemillar@gmail.com>
2017-10-17 14:06:41 -0500

    Button inputs

--- arduboy-hello-world.ino
+++ arduboy-hello-world.ino
@@ -5,10 +5,20 @@ void setup() {
 	// put your setup code here, to run once:
 	arduboy.begin();
 	arduboy.clear();
-	arduboy.print("Hello, World!");
-	arduboy.display();
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
+	arduboy.clear();
+	arduboy.setCursor(10, 10);
+
+	if (arduboy.pressed(A_BUTTON) == true) {
+		arduboy.print("Hello, A button!");
+	} else if (arduboy.pressed(B_BUTTON) == true) {
+		arduboy.print("Hello, B button!");
+	} else {
+		arduboy.print("Hello, World!");
+	}
+
+	arduboy.display();
 }
