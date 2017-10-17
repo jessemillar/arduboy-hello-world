d25fe9490a1d312cdbe28469b267b91eb7bd4f06
Jesse Millar <hellojessemillar@gmail.com>
2017-10-17 13:48:16 -0500

    Initial commit

---
+++ arduboy-hello-world.ino
@@ -0,0 +1,14 @@
+#include <Arduboy.h>
+Arduboy arduboy;
+
+void setup() {
+	// put your setup code here, to run once:
+	arduboy.begin();
+	arduboy.clear();
+	arduboy.print("Hello, World!");
+	arduboy.display();
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+}
