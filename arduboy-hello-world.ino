136b85bac3c5ebb5f4afce1b4c4fa87d519d2ef0
Jesse Millar <hellojessemillar@gmail.com>
2017-10-18 08:37:11 -0500

    Formatting and using Arduboy2 instead

--- arduboy-hello-world.ino
+++ arduboy-hello-world.ino
@@ -1,25 +1,31 @@
-#include <Arduboy.h>
+#include <Arduboy2.h>
 
 #include "images.h"
 
-Arduboy arduboy;
+Arduboy2 arduboy;
 
-void setup() {
+void setup()
+{
 	// put your setup code here, to run once:
 	arduboy.begin();
-	arduboy.clear();
 }
 
-void loop() {
+void loop()
+{
 	// put your main code here, to run repeatedly:
 	arduboy.clear();
 	arduboy.setCursor(10, 10);
 
-	if (arduboy.pressed(A_BUTTON) == true) {
+	if (arduboy.pressed(A_BUTTON) == true)
+	{
 		arduboy.print("Hello, A button!");
-	} else if (arduboy.pressed(B_BUTTON) == true) {
+	}
+	else if (arduboy.pressed(B_BUTTON) == true)
+	{
 		arduboy.print("Hello, B button!");
-	} else {
+	}
+	else
+	{
 		arduboy.print("Hello, World!");
 	}
 
